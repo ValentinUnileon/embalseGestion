a483d7b62ad58d23a3379bcb0304a2e5704fb939
ValentinUnileon <74121671+ValentinUnileon@users.noreply.github.com>
2023-03-12 17:21:23 +0100

    Add files via upload

---
+++ controlEmbalses.java
@@ -0,0 +1,50 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author valen
+ */
+public class controlEmbalses {
+
+
+    
+    
+    public static void main(String[] args) throws SQLException {
+
+        while(true){
+
+            System.out.println("Introduce un comando");
+            Scanner entradaEscaner = new Scanner (System.in);
+            String entradaTeclado = entradaEscaner.nextLine ();
+            System.out.println("Introd"+ entradaTeclado);
+
+            switch(entradaTeclado){
+                case "!salir":
+                    System.out.println("Saliendo del sistema");
+                    break;
+                case "!info":
+
+                    System.out.println("Introduce el embalse");
+                    Scanner entradaEmbalse = new Scanner (System.in);
+                    String entradaInfo = entradaEmbalse.nextLine ();
+                    //busccar embalse
+                    break;
+                case "":
+                
+            }
+
+            
+        }
+        
+         
+       
+
+        
+    }
+    
+}
